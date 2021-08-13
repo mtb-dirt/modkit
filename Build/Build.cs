5d6ae911d9383fb8a9baaa09dc08e3b57cc56b07
Lucas Löffel <lucas@loeffel.io>
2021-08-13 12:29:59 +0200

    update

---
+++ Build/Build.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Build
+{
+    public class Build : EditorWindow
+    {
+        [MenuItem("Modkit/Build", false, 1)]
+        public static void BuildAssetBundle()
+        {
+            Caching.ClearCache();
+            BuildPipeline.BuildAssetBundles("Assets", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        }
+    }
+}
